78148bdf2f3bb2b48a575055566d0c0c56d0346b
Peter Cheng <petercheng00@gmail.com>
2013-04-20 17:59:31 -0700

    line segmentation working, will add interface soon

--- preprocess.m
+++ preprocess.m
@@ -1,14 +1,20 @@
 function [ outIm ] = preprocess( im )
 imbw = im2bw(im, graythresh(im));
+imbw = 1 - imbw;
 
-[height, width] = size(imbw);
-
+[r,c] = find(imbw > 0);
+rowinds = find(abs(r-mean(r)) > 2 * std(r));
+colinds = find(abs(c-mean(c)) > 2 * std(c));
+outlierinds = unique([rowinds;colinds]);
+outlierinds = sub2ind(size(imbw), r(outlierinds), c(outlierinds));
+imbw(outlierinds) = 0;
 
 colSums = sum(imbw,1);
 rowSums = sum(imbw,2);
 
-filledCols = find(colSums < height-3);
-filledRows = find(rowSums < width-3);
+
+filledCols = find(colSums > 0);
+filledRows = find(rowSums > 0);
 
 minCol = min(filledCols);
 maxCol = max(filledCols);
@@ -16,6 +22,6 @@ minRow = min(filledRows);
 maxRow = max(filledRows);
 
 outIm = imbw(minRow:maxRow, minCol:maxCol);
-outIm = 1 - outIm;
+
 end
 
