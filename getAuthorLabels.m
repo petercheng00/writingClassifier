df4f3f4006e94ea45975cc6bbf21216e20b4b486
Peter Cheng <petercheng00@gmail.com>
2013-05-08 19:07:14 -0700

    voting stuff

---
+++ getAuthorLabels.m
@@ -0,0 +1,13 @@
+function [author_labels] = getAuthorLabels(authors, word_labels)
+    author_labels = zeros(size(unique(authors),2),1);
+    [authors_uniq, sortVec] = sort(authors);
+    UV(sortVec) = ([1,diff(authors_uniq)] ~= 0);
+    authors_unique = authors(UV);
+    for i=1:size(author_labels,1);
+        author_num = authors_unique(i);
+        startInd = find(authors==author_num, 1, 'first');
+        endInd = find(authors==author_num, 1, 'last');
+        currentLabels = word_labels(startInd:endInd);
+        author_labels(i) = sum(currentLabels) > length(currentLabels) / 2;           
+    end
+end
