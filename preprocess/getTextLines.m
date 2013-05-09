4cb48735781b9d8ecfc463504536ea34e8d47888
Peter Cheng <petercheng00@gmail.com>
2013-05-09 15:13:15 -0700

    added line images

--- preprocess/getTextLines.m
+++ preprocess/getTextLines.m
@@ -1,10 +1,14 @@
-function lineImages = getTextLines( im )
+function [lineImages, lineAngles] = getTextLines( im )
 lineImages = {};
+lineAngles = [];
 [height, width]= size(im);
 [H,T,R] = hough(im, 'RhoResolution',1,'Theta',[-90:-85,85:89]);
+
+vis = false;
+
 P  = houghpeaks(H,500,'threshold',ceil(0.3*max(H(:))));
 % Find lines and plot them
-lines = houghlines(im,T,R,P,'FillGap',500,'MinLength',0.75*width);
+lines = houghlines(im,T,R,P,'FillGap',0.1*width,'MinLength',0.75*width);
 
 tempIm = im;
 for k = 1:length(lines)
@@ -16,20 +20,52 @@ for k = 1:length(lines)
    yinter = y1 - slope * (x1);
    x = linspace(1,width,100*width);
    y = slope * x + yinter;
+   if vis
+       y = [y-1,y+1,y-2,y+2,y-3,y+3];
+       x = [x, x, x, x, x, x];
+   end
+   y = min(y,height);
+   y = max(y,1);
+
    index = sub2ind(size(im),round(y),round(x));
-   tempIm(index) = 1;
+   tempIm(index) = 1; 
 end
-[labeled] = bwlabel(tempIm);
+if vis
+    imagesc(tempIm)
+    pause
+end
+[labeled, numCC] = bwlabel(tempIm);
+
+CCtoTextLine = zeros(1,numCC);
+
+% first row is sum of all image lines
+% second row is num image lines for this text line
+processedLines = zeros(2,length(lines));
+
 for k = 1:length(lines)
-    x = lines(k).point1(1);
-    y = lines(k).point1(2);
-    l = labeled(y,x);
+    x1 = lines(k).point1(1);
+    y1 = lines(k).point1(2);
+    x2 = lines(k).point2(1);
+    y2 = lines(k).point2(2);
+    l = labeled(y1,x1);
     [r,c] = find(labeled == l);
-    newIm = logical(zeros(size(im)));
-    inds = sub2ind(size(im),r,c);
-    newIm(inds) = im(inds);
-    lineImages{end+1} = newIm(min(r):max(r),min(c):max(c));
+    if size(r) < 10000
+        continue;
+    end
+    
+    if CCtoTextLine(l) == 0    
+        newIm = logical(zeros(size(im)));
+        inds = sub2ind(size(im),r,c);
+        newIm(inds) = im(inds);
+        lineImages{end+1} = newIm(min(r):max(r),min(c):max(c));
+        CCtoTextLine(l) = size(lineImages,2);
+    end
+    angle = rad2deg(atan2(y2-y1,x2-x1));
+    processedLines(1,CCtoTextLine(l)) = processedLines(1,CCtoTextLine(l)) + angle;
+    processedLines(2,CCtoTextLine(l)) = processedLines(2,CCtoTextLine(l)) + 1;
 end
 
+lineAngles = (processedLines(1,:) ./ processedLines(2,:))';
+lineAngles = lineAngles(1:size(lineImages,2));
 end
 
--- preprocess/lineSegmentationToImages.m
+++ preprocess/lineSegmentationToImages.m
@@ -12,25 +12,30 @@ inDir = strcat(inDir, '/');
 inFiles = dir(strcat(inDir,'*_4.jpg'));
 numFiles = size(inFiles,1);
 
+lineAngles = zeros(0,3);
+
 fileCounter = 1;
 multiWaitbar('CLOSEALL');
 for file = inFiles'
+  if fileCounter > 282
+      break;
+  end
   multiWaitbar('Processing Files', fileCounter/numFiles);
   multiWaitbar('Processing Lines', 0);
   origIm = imread(strcat(inDir,file.name));
   [cropIm, cropDim] = preprocess(origIm);
-  boxes = getTextLines(cropIm);
+  [lineImages, fileLineAngles] = getTextLines(cropIm);
   fileId = str2double(file.name(1:end-6));
-  lineCounter = 1;
-  for boxInd = 1:size(boxes,2)
-      multiWaitbar('Processing Lines', boxInd/size(boxes,1));
-      lineIm = cropIm(boxes{boxInd}.minRow:boxes{boxInd}.maxRow,boxes{boxInd}.minCol:boxes{boxInd}.maxCol);
-      fileName = strcat(outputDir,'/',num2str(fileId),'_',num2str(lineCounter),'.bmp');
+  for lineInd = 1:size(lineImages,2)
+      multiWaitbar('Processing Lines', lineInd/size(lineImages,2));
+      lineIm = lineImages{lineInd};
+      fileName = strcat(outputDir,'/',num2str(fileId),'_',num2str(lineInd),'.bmp');
       imwrite(lineIm, fileName);
-      lineCounter = lineCounter + 1;
+      lineAngles(end+1,:) = [fileId, lineInd, fileLineAngles(lineInd)];
   end
   fileCounter = fileCounter + 1;
 end
+csvwrite(strcat(outputDir,'/','lineAngles.csv'),lineAngles);
 multiWaitbar('CLOSEALL');
 end
 
